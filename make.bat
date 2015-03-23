30c2f69c9aac27f14749eddfa634306f72f47a79
Charlie Burrows <~charlie.burrows@asml.com>
2015-03-23 14:05:19 -0400

    Removed some unneeded libraries

--- make.bat
+++ make.bat
@@ -1,8 +1,8 @@
-REM c:\Localdata\gtk2.24-32\bin>pkg-config.exe --cflags --libs gtk-win32-2.0
+REM c:\Localdata\gtk2.24-32\bin\pkg-config.exe --cflags --libs gtk+-2.0
 
 set CFLAGS=-m32 -mms-bitfields -Ic:/Localdata/gtk2.24-32/include/gtk-2.0 -Ic:/Localdata/gtk2.24-32/lib/gtk-2.0/include -Ic:/Localdata/gtk2.24-32/include/atk-1.0 -Ic:/Localdata/gtk2.24-32/include/cairo -Ic:/Localdata/gtk2.24-32/include/gdk-pixbuf-2.0 -Ic:/Localdata/gtk2.24-32/include/pango-1.0 -Ic:/Localdata/gtk2.24-32/include/glib-2.0 -Ic:/Localdata/gtk2.24-32/lib/glib-2.0/include -Ic:/Localdata/gtk2.24-32/include/pixman-1 -Ic:/Localdata/gtk2.24-32/include -Ic:/Localdata/gtk2.24-32/include/freetype2 -Ic:/Localdata/gtk2.24-32/include/libpng14  
 
-set LIBS=-Lc:/Localdata/gtk2.24-32/lib -lgtk-win32-2.0 -lgdk-win32-2.0 -latk-1.0 -lgio-2.0 -lpangowin32-1.0 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lpango-1.0 -lcairo -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lglib-2.0 -lintl
+set LIBS=-Lc:/Localdata/gtk2.24-32/lib -lgtk-win32-2.0 -lgdk-win32-2.0 -lcairo -lgobject-2.0 -lglib-2.0
 
 PATH=c:/localdata/mingw/lib;c:/localdata/mingw/bin;c:/localdata/gtk2.24-32/bin;c:/localdata/gtk2.24-32/lib;%PATH%
 
